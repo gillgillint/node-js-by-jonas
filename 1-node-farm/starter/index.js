fb38147343909c7f41cff9eb5427e040cb6a9ed4
Gill <kantapong.int.work@gmail.com>
2022-08-23 17:55:48 +0700

    ep25

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////////////
 // FILES
@@ -31,24 +33,60 @@ const url = require('url');
 
 ///////////////////////////////////////
 // SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-const pathName = req.url
+  const { query, pathname } = url.parse(req.url, true);
 
-if(pathName === '/' || pathName === '/overview'){
-  res.end('Hello from the overview!');
-} else if(pathName === '/product'){
-  res.end('Hello from the product!');
-} else{
-  res.writeHead(404,{
-    'Contype-type':'text/html',
-    'my-own-header':'hello world'
-  })
-  res.end('<h1> Page ont Found 404! </h1>');
-}
+  // Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+
+    // Product Page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+
+    // 404
+  } else {
+    res.writeHead(404, {
+      'Contype-type': 'text/html',
+      'my-own-header': 'hello world',
+    });
+    res.end('<h1> Page ont Found 404! </h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
   console.log(`Listening to req on port 8000`);
+  console.log(`http://127.0.0.1:8000/`);
 });
-
